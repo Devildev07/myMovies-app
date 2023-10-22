5d1bff36bed206ec8beea8824e34159f6ab80594
Dewanshu Singh <dewanshusingh21@gmail.com>
2023-10-23 00:26:11 +0530

    add movie done

--- src/app/pages/add-data/add-movies/add-movies.page.ts
+++ src/app/pages/add-data/add-movies/add-movies.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PickerController } from '@ionic/angular';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-add-movies',
@@ -10,7 +13,11 @@ export class AddMoviesPage implements OnInit {
   years: number[] = [];
   selectedYear: number | any;
 
-  constructor(public picker: PickerController) {
+  constructor(
+    public picker: PickerController,
+    private agFire: Firestore,
+    private router: Router
+  ) {
     // Generate a range of years, e.g., from 1970 to the current year
     const currentYear = new Date().getFullYear();
     for (let year = 1970; year <= currentYear; year++) {
@@ -28,7 +35,7 @@ export class AddMoviesPage implements OnInit {
           text: 'Cancel',
           role: 'cancel',
         },
-        { 
+        {
           text: 'Confirm',
           handler: (value) => {
             // Handle the selected year (value.year)
@@ -36,7 +43,7 @@ export class AddMoviesPage implements OnInit {
           },
         },
       ],
-      columns: [  
+      columns: [
         {
           name: 'year',
           options: this.years.map((year) => ({
@@ -51,5 +58,20 @@ export class AddMoviesPage implements OnInit {
 
   addData(addMovie: any) {
     console.log(addMovie.value);
+
+    const collectionInst = collection(this.agFire, 'movies');
+    addDoc(collectionInst, addMovie.value)
+      .then(() => {
+        console.log('Data Sent');
+        addMovie.reset();
+
+        // Redirect to the home page after a 5-second delay
+        setTimeout(() => {
+          this.router.navigate(['/home']);
+        }, 5000);
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
   }
 }

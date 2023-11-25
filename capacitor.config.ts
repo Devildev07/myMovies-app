c29ccf418e44a80002434a55229c5996970dd60a
Dewanshu Singh <dewanshusingh21@gmail.com>
2023-11-26 02:41:21 +0530

    Revert "package upgrade"
    This reverts commit 99b3d9ae58b6d2dc29e3c8763432fd049ac3750c.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'myMovies-app',
-  webDir: 'www\browser',
+  webDir: 'www',
   server: {
     androidScheme: 'https'
   }

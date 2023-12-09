1228b97a3475561532c38152b4fdd6ec232543d7
Dewanshu Singh <dewanshusingh21@gmail.com>
2023-12-10 00:05:09 +0530

    profile page updated

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Firestore, collection } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { addDoc } from '@firebase/firestore';
 import { AuthService } from 'src/app/auth/auth.service';
@@ -13,6 +19,9 @@ import { GetDataService } from 'src/app/services/getData/get-data.service';
 })
 export class ProfilePage implements OnInit {
   userEmail: any;
+  profileData: any;
+  profiles: any;
+  profileLength: any = 0;
 
   constructor(
     public authService: AuthService,
@@ -27,15 +36,16 @@ export class ProfilePage implements OnInit {
 
   ngOnInit() {
     this.userEmail = this.authService.userEmail;
+    this.getProfileData();
   }
 
-  updateProfile(profileForm: any) {
-    console.log(profileForm.value);
+  addProfile(addProfileForm: any) {
+    console.log(addProfileForm.value);
     const collectionInst = collection(this.agFire, 'users');
-    addDoc(collectionInst, profileForm.value)
+    addDoc(collectionInst, addProfileForm.value)
       .then(() => {
         console.log('Data Sent');
-        // profileForm.reset();
+        // addProfileForm.reset();
       })
       .catch((err) => {
         console.log('error', err);
@@ -48,5 +58,53 @@ export class ProfilePage implements OnInit {
       this.route.navigate(['/login']);
       this.authService.isUserLogin = false;
     });
+  }
+
+  async getProfileData() {
+    this.profileData = await this.getDatas.getData('users');
+    console.log(this.profileData);
+    const filterProfile = this.profileData.filter((data: any) => {
+      return data.email === this.userEmail;
+    });
+    console.log('filterProfile', filterProfile);
+    if (filterProfile.length > 0) {
+      this.profiles = filterProfile;
+    } else {
+      // If no profile data is found, create an empty profile
+      this.profiles = [{ uname: '', email: '' }];
+    }
+
+    this.profiles = filterProfile;
+    this.profileLength = this.profiles.length;
+  }
+
+  updateProfile(updateProfileForm: any) {
+    const profileId = this.profiles[0].id;
+    console.log('profileId', profileId);
+
+    const profileRef = doc(this.agFire, 'users', profileId);
+
+    const updateProfileData = updateProfileForm.value;
+    console.log('updateProfileData', updateProfileData);
+
+    updateDoc(profileRef, updateProfileData)
+      .then(() => {
+        console.log('Profile data updated successfully!');
+      })
+      .catch((error) => {
+        console.error('Error updating profile data:', error);
+      });
+  }
+
+  deleteProfile() {
+    const profileId = this.profiles[0].id;
+    console.log('profileId', profileId);
+    deleteDoc(doc(this.agFire, 'users', profileId))
+      .then(() => {
+        console.log('Profile deleted successfully!');
+      })
+      .catch((error) => {
+        console.error('Error deleting profile:', error);
+      });
   }
 }

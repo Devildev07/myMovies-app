f2fcd90d1d3c30ebb7b6c144d3d03be54194c1cc
Dewanshu Singh <dewanshusingh21@gmail.com>
2023-12-09 12:30:51 +0530

    profile in progress

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore, collection } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { addDoc } from '@firebase/firestore';
+import { AuthService } from 'src/app/auth/auth.service';
+import { CommonService } from 'src/app/services/common-service/common.service';
+import { GetDataService } from 'src/app/services/getData/get-data.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +12,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  userEmail: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public authService: AuthService,
+    public route: Router,
+    public common: CommonService,
+    public getDatas: GetDataService,
+    public agFire: Firestore
+  ) {
+    var localData = common.getLocalStorage('userData');
+    console.log('localData=', localData);
+    this.userEmail = localData.user.user.email;
   }
 
+  ngOnInit() {}
+
+  updateProfile(profileForm: any) {
+    console.log(profileForm.value);
+    const collectionInst = collection(this.agFire, 'users');
+    addDoc(collectionInst, profileForm.value)
+      .then(() => {
+        console.log('Data Sent');
+        // profileForm.reset();
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
+  }
+
+  async logOut() {
+    this.authService.signOutUser().then(() => {
+      this.common.removeLocalStorage('userData');
+      this.route.navigate(['/login']);
+      this.authService.isUserLogin = false;
+    });
+  }
 }
